d7cf34965515f47adee2b25df8c27448e866fc26
jyjang <jyjang@openit.co.kr>
2025-04-03 11:16:12 +0900

    test : 테스트

--- index.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const formatDay: (date?: string, template?: string) => string;
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import './App.css';
-import PaginationSample from './Pagination/PaginationSample';
-import PopupSample from './Popup/PopupSample';
 
 function App() {
   return (
     <div className="App">
-      <PaginationSample />
-      <PopupSample />
     </div>
   );
 }
